e256a99e6f07df2d14aa2ea31814f3b0bd8f3140
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-05-30 21:18:13 +0500

    work

--- React js 29-5-2024 = cv/my-app/src/App.js
+++ React js 29-5-2024 = cv/my-app/src/App.js
@@ -1,23 +1,99 @@
-import logo from "./logo.svg";
-import img1 from "./images/Animation - 1717016225867.gif";
-
+import React from "react";
 import "./App.css";
+import profilePicture from "./images/profile.jpg"; // Replace with your profile picture path
 
 function App() {
   return (
-    <div className="main">
-      <div
-        className="myApp {
-"
-      >
-        <div className="intro">
-       <h1> Hi, I'm Hasan 👋</h1> 
-       <br />
-       <p>A passionate Full Stack Software Developer 🚀 having an
-        experience of building Web and Mobile applications with JavaScript /
-        Reactjs / Nodejs / React Native and some other cool libraries and
-        frameworks.</p> </div>
-        <img src={img1} alt="Image" />
+    <div className="resume">
+      <div className="left-column">
+        <div className="profile-picture">
+          <img src={profilePicture} alt="Profile" />
+        </div>
+        <h1>Hasan</h1>
+        <h2>Frontend Developer</h2>
+        <div className="section">
+          <h3>Skills</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </div>
+        <div className="section">
+          <h3>Contact</h3>
+          <ul>
+            <li>033333333333</li>
+            <li>smit@gmail.com</li>
+            <li>Karachi Pakistan</li>
+          </ul>
+        </div>
+        <div className="section">
+          <h3>Hobbies & Interest</h3>
+          <ul>
+            <li>Traveling</li>
+            <li>Driving</li>
+            <li>Gaming</li>
+            <li>paragliding</li>
+          </ul>
+        </div>
+
+        <div className="section">
+          <h3>Honor & Award</h3>
+          <p>
+            <p className="y">2024</p>
+           lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
+          </p>
+        </div>
+      </div>
+      <div className="right-column">
+        <div className="section">
+          <h3 className="bg-color">Education</h3>
+          <div className="item">
+            <h4>2022-2024</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
+            </p>
+          </div>
+        </div>
+        <div className="section">
+          <h3 className="bg-color">Work Experience</h3>
+          <div className="item">
+            <h4>2022-2024</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
+            </p>
+          </div>
+          <div className="item">
+            <h4>2022-2024</h4>
+            <h5 className="bg-color">Name of Certification</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
+            </p>
+          </div>
+          <div className="item">
+            <h4>2022-2024</h4>
+            <h5 className="bg-color">Name of Certification</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
+            </p>
+          </div>
+        </div>
+        <div className="section">
+          <h3 className="bg-color">Languages</h3>
+          <ul>
+            <li>
+             
+              English - 60%
+            </li>
+            <li>Urdu - 75%</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
