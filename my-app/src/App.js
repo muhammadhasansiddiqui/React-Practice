ee80095bd8dd50fa7c26a55cc1b89d846347a057
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-06-28 16:16:57 +0500

    work

---
+++ my-app/src/App.js
@@ -0,0 +1,78 @@
+
+import React from 'react'
+
+function App() {
+
+const {list, setList } = useState([])
+
+
+
+
+const SHEET ID = ""
+const API KEY = ""
+const api = ""
+
+
+
+  return (
+    <div>App</div>
+  )
+}
+ 
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+
+// function App() {
+
+// const [count , setCount] = useState(0);
+// const [showbutton , setshowbutton] = useState(true);
+
+// const setting = () => {
+//   var value = count + 1; ;
+//   if(count === 4){
+//     setshowbutton(false);
+//   }
+// }
+
+
+
+
+
+
+//   return (
+//     <div>
+//       <button style={{height:'50px',width:'200px'}} onClick={setting}>
+//         {count}
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
